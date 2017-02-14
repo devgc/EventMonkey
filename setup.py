4a901a2e6f10405b77a8f8868145009773a8e9b3
unknown <mseyer@JOHNNY-B.g-cpartners.local>
2017-02-14 14:02:14 -0600

    setup.py install script and MANIFEST

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    from distutils.core import find_packages, setup
+
+setup(
+    name='eventmonkey',
+    version='1.2.0',
+    description='A Windows Event Processing Utility',
+    author = 'G-C Partners, LLC',
+    author_email = 'dev@g-cpartners.com',
+    url='https://github.com/devgc/EventMonkey',
+    download_url = 'https://github.com/devgc/EventMonkey/archive/1.2.0.tar.gz',
+    license="Apache Software License v2",
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'lxml',
+        'bs4',
+        'progressbar2',
+        'pyyaml'
+    ],
+    packages=find_packages(
+        '.'
+    ),
+    scripts=[
+        u'EventMonkey.py',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+    ]
+)
